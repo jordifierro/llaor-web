11fc463b578b586d53ff13c2b21dff132872e747
jordifierro <jordifierromulero@gmail.com>
2019-07-20 09:51:51 +0200

    Add 404 Notfound behaviour

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './index.scss';
+import NavbarComponent from 'commons/components/navbar/NavbarComponent';
+import NotfoundComponent from 'commons/components/notfound/NotfoundComponent';
+
 import WordView from 'words/views/WordView';
 import WordsByFirstLetterView from 'words/views/WordsByFirstLetterView';
-import NavbarComponent from 'commons/components/navbar/NavbarComponent';
 import { WordApiRepository } from 'words/repositories/WordApiRepository';
 
 const apiHost = process.env['REACT_APP_API_HOST'];
@@ -13,12 +15,15 @@ const apiHost = process.env['REACT_APP_API_HOST'];
 ReactDOM.render(
     <Router>
         <NavbarComponent />
-        <Route path="/diccionari/abc" render={({match}) => (
-            <WordsByFirstLetterView wordApiRepository={new WordApiRepository(apiHost)} /> )}
-        />
-        <Route path="/diccionari/mots/:id" render={({match}) => (
-            <WordView wordId={match.params.id}
-                wordApiRepository={new WordApiRepository(apiHost)} /> )}
-        />
+        <Switch>
+            <Route path="/diccionari/abc" render={({match}) => (
+                <WordsByFirstLetterView wordApiRepository={new WordApiRepository(apiHost)} /> )}
+            />
+            <Route path="/diccionari/mots/:id" render={({match}) => (
+                <WordView wordId={match.params.id}
+                    wordApiRepository={new WordApiRepository(apiHost)} /> )}
+            />
+            <Route component={NotfoundComponent} />
+        </Switch>
     </Router>
 , document.getElementById('root'));
