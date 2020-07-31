5b43d395f8eea31f36c585ff9756e2c1362bf439
Jordi Fierro <jordifierromulero@gmail.com>
2020-07-31 12:44:29 +0200

    Make navbar title link to home

--- src/commons/components/navbar/NavbarComponent.js
+++ src/commons/components/navbar/NavbarComponent.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavbarComponent = () => {
     return (
         <div className="navbar">
-            <h1>llaor</h1>
+            <Link to="/">
+                <h1>llaor</h1>
+            </Link>
         </div>
     );
 };
